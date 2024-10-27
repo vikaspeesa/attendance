f568c1dbed6693610ad999bd279e65b3225b9169
vikaspeesa <vikaspeesa1989@gmail.com>
2024-10-27 18:29:52 +0530

    change the stun server configuration

--- pages/1_Real_Time_Prediction.py
+++ pages/1_Real_Time_Prediction.py
@@ -39,7 +39,14 @@ def video_frame_callback(frame):
         print('Save data to database')
     return av.VideoFrame.from_ndarray(pred_img,format="bgr24")
 
+# webrtc_streamer(key="realtimePrediction",video_frame_callback=video_frame_callback,
+#                  rtc_configuration={
+#         "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
+#     })
+
 webrtc_streamer(key="realtimePrediction",video_frame_callback=video_frame_callback,
-                 rtc_configuration={
-        "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
-    })
+                rtc_configuration={
+                    "iceServers":[{
+                        "urls":["stun:stun.l.google.com:19302"]
+                    }]
+                })
